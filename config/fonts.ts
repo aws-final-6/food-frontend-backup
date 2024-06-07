a701c2327e68eedfa05e65a840ca446abd954715
Alice <jaeyeon0921@gmail.com>
2024-06-07 13:57:12 +0900

    add: gamja font and apply to subtitle

--- config/fonts.ts
+++ config/fonts.ts
@@ -1,4 +1,8 @@
-import { Fira_Code as FontMono, Inter as FontSans } from "next/font/google";
+import {
+  Fira_Code as FontMono,
+  Inter as FontSans,
+  Gamja_Flower as FontGamja,
+} from "next/font/google";
 
 export const fontSans = FontSans({
   subsets: ["latin"],
@@ -8,4 +12,10 @@ export const fontSans = FontSans({
 export const fontMono = FontMono({
   subsets: ["latin"],
   variable: "--font-mono",
+});
+
+export const fontGamja = FontGamja({
+  subsets: ["latin"],
+  variable: "--font-gamja",
+  weight: "400",
 });
