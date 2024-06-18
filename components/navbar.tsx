3b405c65fd11bfdcc19b7b1df52f6636e34eeb77
Alice <jaeyeon0921@gmail.com>
2024-06-18 15:06:08 +0900

    add: navbar

--- components/navbar.tsx
+++ components/navbar.tsx
@@ -20,30 +20,31 @@ import { siteConfig } from "@/config/site";
 import { ThemeSwitch } from "@/components/theme-switch";
 import { SearchIcon } from "@/components/icons";
 import { useTheme } from "next-themes";
+import Searchbar from "./navbar/Searchbar";
 
 export const Navbar = () => {
   const { theme } = useTheme();
-  const searchInput = (
-    <Input
-      aria-label="Search"
-      classNames={{
-        inputWrapper: "bg-default-100",
-        input: "text-sm",
-      }}
-      labelPlacement="outside"
-      placeholder="레시피 검색하기.."
-      startContent={
-        <SearchIcon className="text-base text-default-400 pointer-events-none flex-shrink-0" />
-      }
-      type="search"
-    />
-  );
+  // const searchInput = (
+  //   <Input
+  //     aria-label="Search"
+  //     classNames={{
+  //       inputWrapper: "bg-default-100",
+  //       input: "text-sm",
+  //     }}
+  //     labelPlacement="outside"
+  //     placeholder="레시피 검색하기.."
+  //     startContent={
+  //       <SearchIcon className="text-base text-default-400 pointer-events-none flex-shrink-0" />
+  //     }
+  //     type="search"
+  //   />
+  // );
 
   return (
     <NextUINavbar maxWidth="xl" position="sticky" height={"100px"}>
       <NavbarContent className="basis-1/5 sm:basis-full" justify="start">
-        <NavbarBrand className="">
-          <NextLink className="flex justify-start items-center pt-10" href="/">
+        <NavbarBrand>
+          <NextLink className="flex justify-start items-center" href="/">
             <Image
               src={
                 theme === "light" || theme == null
@@ -56,13 +57,13 @@ export const Navbar = () => {
             />
           </NextLink>
         </NavbarBrand>
-        <ul className="hidden lg:flex gap-4 justify-start ml-2">
+        <ul className="hidden lg:flex gap-12 justify-start ml-2">
           {siteConfig.navItems.map((item) => (
             <NavbarItem key={item.href}>
               <NextLink
                 className={clsx(
                   linkStyles({ color: "foreground" }),
-                  "data-[active=true]:text-primary data-[active=true]:font-medium font-gamja text-xl"
+                  "data-[active=true]:text-primary data-[active=true]:font-medium font-jua text-xl"
                 )}
                 color="foreground"
                 href={item.href}
@@ -78,7 +79,9 @@ export const Navbar = () => {
         className="hidden sm:flex basis-1/5 sm:basis-full"
         justify="end"
       >
-        <NavbarItem className="hidden lg:flex">{searchInput}</NavbarItem>
+        <NavbarItem className="hidden lg:flex">
+          <Searchbar />
+        </NavbarItem>
 
         <NavbarItem className="hidden md:flex">
           <Button
@@ -101,7 +104,7 @@ export const Navbar = () => {
       </NavbarContent>
 
       <NavbarMenu>
-        {searchInput}
+        <Searchbar />
         <div className="mx-4 mt-2 flex flex-col gap-2">
           {siteConfig.navMenuItems.map((item, index) => (
             <NavbarMenuItem key={`${item}-${index}`}>
