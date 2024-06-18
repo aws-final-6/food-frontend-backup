9c5b0935ba568c3ece68dece6916d2dcccf89f92
Alice <jaeyeon0921@gmail.com>
2024-06-18 15:04:22 +0900

    add: font-jua

--- config/fonts.ts
+++ config/fonts.ts
@@ -2,6 +2,7 @@ import {
   Fira_Code as FontMono,
   Inter as FontSans,
   Gamja_Flower as FontGamja,
+  Jua as FontJua,
 } from "next/font/google";
 
 export const fontSans = FontSans({
@@ -17,5 +18,11 @@ export const fontMono = FontMono({
 export const fontGamja = FontGamja({
   subsets: ["latin"],
   variable: "--font-gamja",
+  weight: "400",
+});
+
+export const fontJua = FontJua({
+  subsets: ["latin"],
+  variable: "--font-jua",
   weight: "400",
 });
