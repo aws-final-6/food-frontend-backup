233893c1a999a25f00a4a6f8cd3c6eba8e94efef
Alice <jaeyeon0921@gmail.com>
2024-06-18 15:00:49 +0900

    add: image url for cache imaged

--- next.config.js
+++ next.config.js
@@ -26,6 +26,10 @@ const nextConfig = {
         protocol: "https",
         hostname: "blogfiles.pstatic.net",
       },
+      {
+        protocol: "https",
+        hostname: "recipe1.ezmember.co.kr",
+      },
     ],
   },
 };
