28fcdeaf4f26eba0a727dda4de057587d6577f10
Alice <jaeyeon0921@gmail.com>
2024-06-19 18:49:33 +0900

    update: signup page

---
+++ app/signup/action.tsx
@@ -0,0 +1,35 @@
+"use server";
+const API_URL = process.env.API_URL;
+
+export async function SignupAPI(formData: FormData) {
+  const userInfo = {
+    user_id: formData.get("id"),
+    user_email: formData.get("email"),
+    user_nickname: formData.get("nickname"),
+    user_provider: formData.get("provider"),
+    user_subscription: formData.get("subscription"),
+    user_prefer: [
+      {
+        cate_no: formData.get("prefer_one"),
+        situ_no: formData.get("prefer_two"),
+      },
+    ],
+  };
+  console.log(userInfo);
+  try {
+    const response = await fetch(`${API_URL}/auth/signup`, {
+      method: "POST",
+      body: JSON.stringify(userInfo),
+      headers: {
+        "content-type": "application/json",
+      },
+    });
+
+    const responseData = await response.status;
+    console.log(responseData);
+    return responseData;
+  } catch (error) {
+    console.error("Error:", error);
+    return "회원가입 실패했습니다.";
+  }
+}
