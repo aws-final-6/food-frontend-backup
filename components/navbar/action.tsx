bde8e69631e11dc96652a1a97846c611f00a7787
Alice <jaeyeon0921@gmail.com>
2024-06-21 13:23:18 +0900

    remove: unused api

--- components/navbar/action.tsx
+++ components/navbar/action.tsx
@@ -1,21 +1,6 @@
 "use server";
-import { promises as fs } from "fs";
-import path from "path";
 
 const API_URL = process.env.API_URL;
-
-export async function Datahandler() {
-  const filePath = path.join(process.cwd(), "data.txt");
-  const fileContents = await fs.readFile(filePath, "utf-8");
-  const data = fileContents
-    .split("\n")
-    .map((line) => line.trim())
-    .filter((line) => line.length > 0);
-
-  //console.log(data);
-
-  return data;
-}
 
 export async function SearchRecipeAPI(keyword: string) {
   const recipeName = {
