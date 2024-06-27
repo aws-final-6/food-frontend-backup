f59feea56a5e300b1da31127257f124e0ae0781d
Alice <jaeyeon0921@gmail.com>
2024-06-28 07:45:43 +0900

    add: bookmark api

--- components/auth/check-auth.tsx
+++ components/auth/check-auth.tsx
@@ -1,8 +1,10 @@
 "use client";
+
 import React, { useContext, useEffect } from "react";
 import { useSearchParams } from "next/navigation";
 import { UserContext } from "@/app/providers";
 import { useRouter } from "next/navigation";
+import { getBookmark } from "./action";
 
 const CheckAuth = () => {
   const searchParams = useSearchParams();
@@ -15,12 +17,30 @@ const CheckAuth = () => {
   const is_new = searchParams.get("new");
 
   useEffect(() => {
+    const fetchDataAndSetUserData = async (userId: string) => {
+      try {
+        const favorite = await getBookmark(userId); // Wait for the promise to resolve
+        updateUserData({
+          id: userId,
+          accessToken: access_token || "",
+          refreshToken: refresh_token || "",
+          favorite: favorite || [], // Initialize as empty array if favorite is undefined
+        });
+
+        // Redirect based on is_new flag
+        if (is_new === "true") {
+          router.push("/signup");
+        } else {
+          router.push("/");
+        }
+      } catch (error) {
+        console.error("Error fetching bookmark:", error);
+        // Handle error appropriately (e.g., show error message)
+      }
+    };
+
     if (user_id && access_token && refresh_token) {
-      updateUserData({
-        id: user_id,
-        accessToken: access_token,
-        refreshToken: refresh_token,
-      });
+      fetchDataAndSetUserData(user_id);
       if (is_new == "true") {
         router.push("/signup");
       } else {
